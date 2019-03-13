e827ba6fdf7e88093717cc50d553cf5236b05ce6
student <student@hubu.edu.cn>
2019-03-13 11:27:25 +0800

    20190313

---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div calss="container">
+    <x-headr :left-option="{showback: ture}">登陆</x-headr>
+    <group title="用户">
+      <x-input title="手机号" mask="" v-model="phone" :max="13" is-type="china-mobile"></x-input>
+      <x-input title="密码" type="password" v-model="password" :max="13" is-type="china-mobile"></x-input>
+  </div class="btn">
+</template>
+
+<script>
+import { XInput, Group, XButton, Cell , XHeader, AlertModule, Alter} from 'vux'
+
+export default {
+  components: {
+    XInput,
+    XButton,
+    Group,
+    Cell
+  },   
+</script>
+</style>
--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ import FastClick from 'fastclick'
 import VueRouter from 'vue-router'
 import App from './App'
 import home from './components/home'
+import Login from './components/Login'
 
 Vue.use(VueRouter)
-
 const routes = [{
   path: '/',
   component: home
+}, {
+  path: '/login',
+  component: Login
 }]
 
 const router = new VueRouter({
